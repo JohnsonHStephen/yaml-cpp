f63aab0e556d185148fab79b77cbd164ecccca5d
Stephen Johnson <47369481+JohnsonHStephen@users.noreply.github.com>
2024-01-29 19:33:52 -0500

    Added premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,42 @@
+project "Hazel"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+  
+		"include/**.h",
+	}
+
+	includedirs
+	{
+		"include",
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "system:linux"
+		pic "on"
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		runtime "Release"
+		optimize "on"
